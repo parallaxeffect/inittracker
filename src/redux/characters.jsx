0b4c194ee642999a10464b94229745f65005cafd
Cassie <parallaxeffect@gmail.com>
2017-10-05 20:21:19 -0400

    add shortid uids

--- src/redux/characters.jsx
+++ src/redux/characters.jsx
@@ -1,16 +1,20 @@
+//import {shortid} from 'shortid'
+var shortid = require('shortid')
+
 var initialState = {characters: ["Mailee"]}
 
 export var reducer = function (state = initialState, action) {
   switch (action.type) {
     case 'ADD_CHARACTER':
-      return {characters: ["Mailee", "Sazh"]}
+      return {characters: ["Mailee", action.id]}
     default:
       return state
   }
 }
 
 export var addCharacter = function () {
-  return {type: "ADD_CHARACTER"}
+  return {type: "ADD_CHARACTER",
+          id: shortid.generate()}
 }
 
 export var listCharacters = (state) => {
